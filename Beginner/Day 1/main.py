d1fbcf1bee41172e0f8e966fecf35c49f8521c45
Emil Moquete <71798519+emmoscript@users.noreply.github.com>
2023-04-30 00:46:51 -0400

    Update main.py

---
+++ Beginner/Day 1/main.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your program.
+greeting = "Welcome to the band name generator, answer the following questions please: "
+
+#2. Ask the user for the city that they grew up in.
+
+city = input("What's the name of the city you grew up in? ")
+
+#3. Ask the user for the name of a pet.
+
+pet = input("What's your pet's name? ")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+result = city + " " + pet
+
+#5. Make sure the input cursor shows on a new line:
+
+print("Your band name could be " + result)
